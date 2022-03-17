51522d8c8d1ccdc19efeb20b7ac46021246fd1b8
junbin <540965117@qq.com>
2022-03-17 23:11:42 +0800

    完善功能

--- client/src/components/TransactionForm.tsx
+++ client/src/components/TransactionForm.tsx
@@ -3,19 +3,22 @@ import TextField from '@mui/material/TextField'
 import Autocomplete from '@mui/material/Autocomplete'
 import LoadingButton from '@mui/lab/LoadingButton'
 import Stack from '@mui/material/Stack'
-import Button from '@mui/material/Button'
 import { TransactionContext } from '../context/TransactionContext'
 import type { FormDataType } from '../types'
 
 function TransactionForm() {
-  const { currentAccount, formData, setFormData, sendTransaction, isLoading } =
-    useContext(TransactionContext)
+  const { formData, setFormData, sendTransaction, isLoading } = useContext(TransactionContext)
+
   function handleChange(
     event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
     key: keyof FormDataType
   ) {
     const value = event.target.value
-    setFormData((prevState) => ({ ...prevState, [key]: value }))
+    const data = {
+      ...formData,
+      [key]: value,
+    }
+    setFormData(data)
   }
 
   async function handleSubmit() {
@@ -25,7 +28,6 @@ function TransactionForm() {
   }
   return (
     <div className='w-[30rem] rounded-md bg-[#111625] p-3 border border-slate-500'>
-      {isLoading ? '加载中...' : '没有'}
       <div className='pb-2 text-lg'>Swap</div>
       <Stack spacing={2}>
         <Autocomplete
